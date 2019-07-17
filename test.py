15b351879c6b6dccca9db1e04bc97c9d2973cf55
Albert Yang <ay1089@nyu.edu>
2019-07-16 20:27:34 -0400

    uncommented some code in test.py

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from EdgarParser import EdgarParser
 ciks = ['0001756111', '0001166559', '0001555283', '0001397545', '0001543160', '0001496147', '0001357955', '0001439289', '0001086364']
 
 
-'''for cik in ciks:
+for cik in ciks:
     ep = EdgarParser(cik=cik, get_last_holding=True)
     ep.fund_holdings_to_tsv()
 
-'''
+
 EdgarParser(cik='BLK').fund_holdings_to_tsv()
